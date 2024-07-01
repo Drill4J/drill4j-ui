6e8f2b8c4c5a3922395c983588f9cd44841b7caf
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-07-01 19:56:39 +0400

    deps: add antd, axios, react-hook-form, react-router-dom feat: remove web-vitals feat: remove default cra styles

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Drill4J UI h1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Drill4J UI/i);
   expect(linkElement).toBeInTheDocument();
 });
