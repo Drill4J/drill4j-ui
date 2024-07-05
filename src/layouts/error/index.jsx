d7fbd47d76fcdf7aa7f451c92140b25d55c9dc3d
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-07-05 18:57:32 +0400

    chore: remove unused imports

--- src/layouts/error/index.jsx
+++ src/layouts/error/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Layout, Card } from 'antd';
 
 const { Content } = Layout;
--- src/pages/account/my-api-keys/index.jsx
+++ src/pages/account/my-api-keys/index.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const MyApiKeys = () => {
   return (
     <div>
