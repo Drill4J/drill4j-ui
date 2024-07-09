7238fe751f80dd27d7d95d49893644e8b75f7b5f
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-07-09 19:39:41 +0400

    fix: my api keys add vertical gap

--- src/pages/account/my-api-keys/index.jsx
+++ src/pages/account/my-api-keys/index.jsx
@@ -5,15 +5,16 @@ import { useState } from "react";
 const MyApiKeys = () => {
   const [refreshFlag, refreshData] = useState("")
   return (
-    <div>
+    <>
       <GenerateApiKeyForm
         refreshData = {() => refreshData(Date.now().toString())}
       />
+      <div style={{marginTop:'15px'}} />
       <UserApiKeysTable
         refreshData = {() => refreshData(Date.now().toString())}
         refreshFlag = {refreshFlag}
       />
-    </div>
+    </>
   );
 };
 
